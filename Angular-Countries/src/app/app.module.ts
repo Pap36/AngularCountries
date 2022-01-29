6984bb9dbda94afae796bfb12ca160d31f3ab8bc
Paul-Andrei Petan <pap36@bath.ac.uk>
2022-01-29 19:23:30 +0000

    initial structure

--- Angular-Countries/src/app/app.module.ts
+++ Angular-Countries/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { DarkModeComponent } from './dark-mode/dark-mode.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { FilterComponent } from './filter/filter.component';
+import { CountriesListComponent } from './countries-list/countries-list.component';
+import { CountryCardComponent } from './country-card/country-card.component';
+import { CountryPageComponent } from './country-page/country-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    DarkModeComponent,
+    HomepageComponent,
+    SearchbarComponent,
+    FilterComponent,
+    CountriesListComponent,
+    CountryCardComponent,
+    CountryPageComponent
   ],
   imports: [
     BrowserModule,
